c4b3d725ba088b2dc9b22fb209ad4b31e277ffe2
Pepijn te Winkel <37923@ma-web.nl>
2023-11-21 14:56:35 +0100

    second commit

--- 0A1/app.js
+++ 0A1/app.js
@@ -1,7 +1,20 @@
 class App{
     runApplication(){
+        let appNaam = "Pepijn's App";
+        let versieNummer = 0.1;
+        let versieDatum = "11/21/2023";
+        let autheur = "Pepijn te Winkel";
+        let copyright = "Pepijn.co";
+        let distributeur = "Pepijn.sons";
+        let darkmode = false;
         console.log("hello world!");
-        //code gaat hier
+        console.log("appNaam: "+appNaam);
+        console.log("versieNummer: "+versieNummer);
+        console.log("versieNummer: "+versieDatum);
+        console.log("autheur: "+autheur);
+        console.log("copyright: "+copyright);
+        console.log("distributeur: "+distributeur);
+        console.log("darkmode: "+darkmode);
     }
 }
 let app = new App();
