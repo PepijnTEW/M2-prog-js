d0b525ac59f41e92d66eddd65693a7953d9f524b
Pepijn te Winkel <37923@ma-web.nl>
2023-11-21 15:30:59 +0100

    second version

---
+++ 01B/app.js
@@ -0,0 +1,23 @@
+class App{
+    runApplication(){
+        this.greeting = "Starting up";
+        this.appNaam = "Pepijn's App";
+        this.versieNummer = 0.1;
+        this.versieDatum = "11/21/2023";
+        this.autheur = "Pepijn te Winkel";
+        this.copyright = "Pepijn.co";
+        this.distributeur = "Pepijn.sons";
+        this.darkmode = false;
+    }
+}
+let app = new App();
+app.runApplication();
+console.log(app.greeting);
+console.log("hello world!");
+console.log("appNaam: "+app.appNaam);
+console.log("versieNummer: "+app.versieNummer);
+console.log("versieNummer: "+app.versieDatum);
+console.log("autheur: "+app.autheur);
+console.log("copyright: "+app.copyright);
+console.log("distributeur: "+app.distributeur);
+console.log("darkmode: "+app.darkmode);
