75c2022a985ab65fe66bef1d7efe5f2876d09e48
Pepijn te Winkel <37923@ma-web.nl>
2023-11-23 14:48:43 +0100

    M2 Les2 Opdacht1

---
+++ 2.2/02 canvashuis/app.js
@@ -0,0 +1,10 @@
+class App{
+    runApplication(){
+        console.log("Hello World")
+        let canvas = document.getElementById("canvasID")
+        console.log(canvas)
+    }
+}
+let app = new App();
+app.runApplication();
+
