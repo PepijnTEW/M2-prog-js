2679d55a4b0fba4718327f1195cd8cfb5efddd7f
Pepijn te Winkel <37923@ma-web.nl>
2023-11-23 14:18:18 +0100

    Opdrachten 01A/01B

---
+++ 2.1/01B/app.js
@@ -0,0 +1,36 @@
+class App{
+    runApplication(){
+        this.greeting = "Starting up";
+        this.appNaam = "Pepijn's App";
+        this.versieNummer = 0.1;
+        this.versieDatum = "11/21/2023";
+        this.autheur = "Pepijn te Winkel";
+        this.copyright = "Pepijn.co";
+        this.distributeur = "Pepijn.sons";
+        this.darkmode = false;
+        this.classboolean = true
+        this.classString = "een string"
+        this.classnumber = "999"
+        let eenBoolean = true
+        let eenString = "een string"
+        let number = "999"
+        console.log("eenBoolean: "+eenBoolean);
+        console.log("eenString: "+eenString);
+        console.log("number: "+number);
+    }
+}
+let app = new App();
+app.runApplication();
+
+console.log(app.greeting);
+console.log("hello world!");
+console.log("appNaam: "+app.appNaam);
+console.log("versieNummer: "+app.versieNummer);
+console.log("versieNummer: "+app.versieDatum);
+console.log("autheur: "+app.autheur);
+console.log("copyright: "+app.copyright);
+console.log("distributeur: "+app.distributeur);
+console.log("darkmode: "+app.darkmode);
+console.log("classboolean: "+app.classboolean);
+console.log("classString: "+app.classString);
+console.log("classNum: "+app.classnumber);
