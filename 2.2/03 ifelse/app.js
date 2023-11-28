726108196f0ea49779d46e61476948493d02996d
Pepijn te Winkel <37923@ma-web.nl>
2023-11-28 14:25:17 +0100

    M2 Les 3 opdracht 1

---
+++ 2.2/03 ifelse/app.js
@@ -0,0 +1,11 @@
+class App{
+    runApplication(){
+        console.log("Hello World");
+        let newsTitle = document.getElementById("newstitle");
+        let gameHead = document.getElementsByClassName("gamenews headline");
+        let gameNews = document.getElementsByClassName("gamenews")
+    }
+}
+let app = new App();
+app.runApplication();
+
